a6a6098d741073befdb6aaf4b2040b17de739647
safdar211ali <safdar211@hotmail.com>
2025-01-23 18:55:37 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Payment Voucher2
+
+this copy of payment voucher
+
+#### License
+
+MIT
---
+++ payment_voucher2/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Payment Voucher2",
+			"type": "module",
+			"label": _("Payment Voucher2")
+		}
+	]
