39c617a6c58a189d3361233c440cb8899b62673c
SafdarAliGit <safdar211@gmail.com>
2025-05-14 20:05:09 +0500

    updated

--- payment_voucher2/payment_voucher2/doctype/funds_transfer_voucher/funds_transfer_voucher.py
+++ payment_voucher2/payment_voucher2/doctype/funds_transfer_voucher/funds_transfer_voucher.py
@@ -109,14 +109,18 @@ class FundsTransferVoucher(Document):
             frappe.throw("Account type not found")
 
     def on_cancel(self):
-        current_je = get_doctype_by_field('Journal Entry', 'slip_no', self.name)
-        if current_je.docstatus != 2:  # Ensure the document is in the "Submitted" state
-            current_je.cancel()
-            frappe.db.commit()
-        else:
-            frappe.throw("Document is not in the 'Submitted' state.")
-        if current_je.amended_from:
-            new_name = int(current_je.name.split("-")[-1]) + 1
-        else:
-            new_name = f"{current_je.name}-{1}"
-        make_autoname(new_name, 'Jouranl Entry')
+        try:
+            self.ft_status = 0
+            current_je = get_doctype_by_field('Journal Entry', 'slip_no', self.name)
+            if current_je.docstatus != 2:  # Ensure the document is in the "Submitted" state
+                current_je.cancel()
+                frappe.db.commit()
+            else:
+                frappe.throw("Document is not in the 'Submitted' state.")
+            if current_je.amended_from:
+                new_name = int(current_je.name.split("-")[-1]) + 1
+            else:
+                new_name = f"{current_je.name}-{1}"
+            make_autoname(new_name, 'Jouranl Entry')
+        except Exception as e:
+            frappe.throw(f"Error canceling Funds Transfer Voucher {self.name}: {str(e)}")
